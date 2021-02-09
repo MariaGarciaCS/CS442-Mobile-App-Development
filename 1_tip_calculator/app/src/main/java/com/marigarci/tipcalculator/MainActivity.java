d8176c1703e5cb83b1a1a50c57a811e94403355c
Maria Garcia <mgarcia18@hawk.iit.edu>
2021-02-09 16:39:04 -0600

    modified onGoClicked

--- 1_tip_calculator/app/src/main/java/com/marigarci/tipcalculator/MainActivity.java
+++ 1_tip_calculator/app/src/main/java/com/marigarci/tipcalculator/MainActivity.java
@@ -6,26 +6,65 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-    public double tipPercent;
+    private EditText billTotal;
+    private EditText numPeople;
+    private TextView tipAmount;
+    private TextView totalWithTip;
+    private TextView totalPerPerson;
+    private TextView overage;
+    public double billTotalVal;
+    public double tipPercentVal;
+    public double tipAmountVal;
+    public double totalWithTipVal;
+    public int numPeopleVal;
+    public double totalPerPersonVal;
+    public double overageVal;
+
+    public DecimalFormat df2 = new DecimalFormat("#.##");
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        billTotal = findViewById(R.id.billTotalValue);
+        numPeople = findViewById(R.id.numPeopleValue);
+        tipAmount = findViewById(R.id.tipAmountValue);
+        totalWithTip = findViewById(R.id.totalWithTipValue);
+        totalPerPerson = findViewById(R.id.totalPerPersonValue);
+        overage = findViewById(R.id.overageValue);
     }
 
     public void onRadioButtonClicked(View v){
-        if(v.getId() == R.id.twelvePercent){tipPercent = .12; }
-        else if(v.getId() == R.id.fifteenPercent){tipPercent = .15;}
-        else if(v.getId() == R.id.eighteenPercent){tipPercent = .18;}
-        else if(v.getId() == R.id.twentyPercent){tipPercent = .2;}
+        if(v.getId() == R.id.twelvePercent){tipPercentVal = .12; }
+        else if(v.getId() == R.id.fifteenPercent){tipPercentVal = .15;}
+        else if(v.getId() == R.id.eighteenPercent){tipPercentVal = .18;}
+        else if(v.getId() == R.id.twentyPercent){tipPercentVal = .2;}
     }
 
     public void onGoClicked(View v){
+        //Get User Input
+        billTotalVal = Double.parseDouble(billTotal.getText().toString());
+        numPeopleVal = Integer.parseInt(numPeople.getText().toString());
 
+        //Calculate Values
+        tipAmountVal = calcTip(tipPercentVal, billTotalVal);
+        totalWithTipVal = calcTotalWTip(tipAmountVal, billTotalVal);
+        totalPerPersonVal = calcTotalPerPerson(numPeopleVal, billTotalVal);
+        overageVal = calcOverage(numPeopleVal, totalPerPersonVal, billTotalVal);
+
+        //Display Values
+        tipAmount.setText(df2.format(tipAmountVal));
+        totalWithTip.setText(df2.format(totalWithTipVal));
+        totalPerPerson.setText(df2.format(totalPerPersonVal));
+        overage.setText(df2.format(overageVal));
     }
 
     public void onClearClicked(View v){
@@ -41,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
     public double calcTotalPerPerson(int numPeople, double bill){
         return 0;
     }
-    public double calcOverage(int numPeople, double billTotal){
+    public double calcOverage(int numPeople, double costPerPerson, double billTotal){
         return 0;
     }
 }
