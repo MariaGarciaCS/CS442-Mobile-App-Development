e4b2e3feac41e5734f39cdc0fdb052ad5efed97a
Maria <mgarcia18@hawk.iit.edu>
2021-03-02 23:40:55 -0600

    options menu added

--- 3_stock_assistant/app/src/main/java/com/marigarci/stockassistant/MainActivity.java
+++ 3_stock_assistant/app/src/main/java/com/marigarci/stockassistant/MainActivity.java
@@ -1,8 +1,12 @@
 package com.marigarci.stockassistant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Options Menu---------------------------
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.addStockBtn){
+            //TODO: Alert Dialog to add stock, only capital letters
+            Toast.makeText(this, "Add Stock pressed", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
