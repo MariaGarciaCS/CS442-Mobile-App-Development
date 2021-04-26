b637630ef8b55aebbf7c9b6dac7528f385165500
Maria <mgarcia18@hawk.iit.edu>
2021-04-26 01:25:50 -0500

    about page layout done

--- 4_civic_advocacy/app/src/main/java/com/marigarci/civicadvocacy/AboutActivity.java
+++ 4_civic_advocacy/app/src/main/java/com/marigarci/civicadvocacy/AboutActivity.java
@@ -1,4 +1,13 @@
 package com.marigarci.civicadvocacy;
 
-public class AboutActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
 }
